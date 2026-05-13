b29195e265e3aeb6b860bceab0451e53d35cb88e
damstrivandrum <aajalms@gmail.com>
2026-05-13 13:16:43 +0530

    Update exam end time in config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ window.EXAM_TZ = "Asia/Kolkata";
 
 // ✅ Fixed window (IST)
 window.EXAM_START_IST = "2026-05-13T10:00:00+05:30";
-window.EXAM_END_IST   = "2026-05-13T13:15:00+05:30";
+window.EXAM_END_IST   = "2026-05-13T14:15:00+05:30";
 
 // Parsed milliseconds (used by pages)
 window.EXAM_START_MS = Date.parse(window.EXAM_START_IST);
